eb76ba721462d5804dc3064f380b11161573fcc1
Alex Clements <alexfjclements@gmail.com>
2018-11-07 16:43:36 +0000

    hogwards student register completed

--- students_controller.rb
+++ students_controller.rb
@@ -6,14 +6,51 @@ also_reload('./models/*')
 
 # index
 
-# show
+get '/students' do
+  @students = Student.all
+  erb(:students)
+end
 
-# new
+# New
+
+get '/students/new' do
+  erb(:new)
+end
 
 # create
 
+post '/students' do
+  Student.new(params).save
+  redirect to 'students'
+end
+
 # edit
+
+get '/students/:id/edit' do
+  @houses = House.all
+  @student = Student.find(params[:id])
+  erb(:edit)
+end
 
 # update
 
+post '/students/:id' do
+  student = Student.new(params)
+  student.update
+  redirect to 'students/' + params[:id]
+end
+
 # destroy
+
+post '/students/:id/delete' do
+  @student = Student.find(params[:id])
+  @student.delete
+  redirect to 'students'
+end
+
+# Show
+
+get '/students/:id' do
+  @students = Student.find(params[:id])
+  erb(:show)
+end
